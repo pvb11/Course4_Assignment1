fc96b5217dbab467b1aea9582c90e35f839cf0a8
pvb11 <40701656+pvb11@users.noreply.github.com>
2019-05-06 08:16:24 -0400

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,28 @@
+# Load the data
+data <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+
+# Format the data
+timedate <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+data <- cbind(data, timedate)
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Time <- format(data$Time, format="%H:%M:%S")
+data$Global_active_power <- as.numeric(data$Global_active_power)
+data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+data$Voltage <- as.numeric(data$Voltage)
+data$Global_intensity <- as.numeric(data$Global_intensity)
+data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+subdata <- subset(data, Date == "2007-02-01" | Date =="2007-02-02")
+
+# Draw the plot
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+with(subdata, plot(timedate, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+with(subdata, plot(timedate, Voltage, type = "l", xlab="datetime", ylab="Voltage"))
+with(subdata, plot(timedate, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+lines(subdata$timedate, subdata$Sub_metering_2,type="l", col= "red")
+lines(subdata$timedate, subdata$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+with(subdata, plot(timedate, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
+dev.off()
